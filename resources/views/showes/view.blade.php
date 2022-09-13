8fdf82d81906e9b2a288e66be74c5f812df81969
RakadDwairi8 <rakad@newaeon-development.com>
2022-09-13 16:32:04 +0300

    u

--- resources/views/showes/view.blade.php
+++ resources/views/showes/view.blade.php
@@ -12,7 +12,11 @@
         </style>
     @endpush
 
-    <div class="hero mv-single-hero" style="background: url('{{$episode->background_cover}}') no-repeat">
+    <div class="hero mv-single-hero" style="background: url('{{$episode->background_cover}}');  no-repeat center center fixed; 
+        -webkit-background-size: cover;
+        -moz-background-size: cover;
+        -o-background-size: cover;
+        background-size: cover;">
         <div class="container">
             <div class="row">
                 <div class="col-md-12">
